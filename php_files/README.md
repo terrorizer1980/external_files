deff45b888c3fa8ab9a616ed10894c1f443d196e
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-04-07 18:45:02 -0300

    test

---
+++ php_files/README.md
@@ -0,0 +1,1 @@
+# Output files drom CI/CD
--- workflows/php_get.js
+++ workflows/php_get.js
@@ -1,28 +1,34 @@
 const {execSync} = require("child_process")
 const { resolve } = require("path")
 const AdmZip = require("adm-zip");
-const { readFileSync, writeFileSync } = require("fs");
-
+const { readFileSync, writeFileSync, mkdirSync } = require("fs");
+const {exportVariable} = require("@actions/core")
 const php_json = resolve(__dirname, "..", "php_bin.json")
 const repo_published = JSON.parse(readFileSync(php_json, "utf8"))
 const jekins_url_base = "https://jenkins.pmmp.io/job/PHP-7.4-Aggregate"
 const JekinsJSON = JSON.parse(execSync(`curl -sS "${jekins_url_base}/lastSuccessfulBuild/api/json"`))
 if (JekinsJSON.displayName !== repo_published.release){
-    repo_published.release = JekinsJSON.displayName
+    exportVariable("upload", true)
     for (let index in JekinsJSON.artifacts){
         let url_file = `${jekins_url_base}/lastSuccessfulBuild/artifact/${JekinsJSON.artifacts[index].relativePath}`
         let file_name = JekinsJSON.artifacts[index].relativePath
-        if (file_name.includes(".zip")) execSync(`curl -sS "${url_file}" --output /tmp/${file_name}`);
+        repo_published.release = JekinsJSON.displayName
+        let ZIPFileName = file_name.split(".tar.gz").join(".zip")
+        let ZIPFilePath = resolve(__dirname, "..", "php_files",ZIPFileName)
+        let Path = resolve("/tmp", `php_bin_${Math.trunc(Math.random() * 10000000)}`)
+        if (file_name.includes("debug")) console.log(`Skip: ${ZIPFileName}`);
         else {
-            let ZIPFileName = file_name.split(".tar.gz").join(".zip")
-            let ZIPFilePath = resolve(__dirname, "..", ZIPFileName)
-            let Path = resolve("/tmp", `php_bin_${Math.trunc(Math.random * 10000000)}`)
-            execSync(`curl -sS "${url_file}" | tar xfz - -C "${Path}"`)
-            let zip = new AdmZip();
-            zip.addLocalFolder(Path);
-            zip.writeZip(ZIPFilePath);
-            repo_published[ZIPFileName.replaceAll(".zip", "")] = `https://raw.githubusercontent.com/The-Bds-Maneger/Raw_files/main/${ZIPFileName}`
+            let JSon_object = ZIPFileName.replaceAll(".zip", "").replaceAll("PHP-7.4-", "");
+            repo_published[JSon_object] = `https://github.com/The-Bds-Maneger/Bds-Maneger/releases/download/${process.env.tag_name}/${ZIPFileName}`
+            if (file_name.includes(".zip")) execSync(`curl -sS "${url_file}" --output "${ZIPFilePath}"`)
+            else {
+                mkdirSync(Path)
+                execSync(`curl -sS "${url_file}" | tar xfz - -C "${Path}"`)
+                let zip = new AdmZip();
+                zip.addLocalFolder(Path);
+                zip.writeZip(ZIPFilePath);
+            }
         }
+        writeFileSync(php_json, JSON.stringify(repo_published, null, 4))
     }
-    writeFileSync(php_json, JSON.stringify(repo_published, null, 4))
-} else console.log("Jupping pocketmine-PHP");
+} else {console.log("Jupping pocketmine-PHP");exportVariable("upload", false)};
