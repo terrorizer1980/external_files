76160072cf54e11facc57db361a4f92fc880eee0
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-04-19 19:48:50 -0300

    test

--- phpBinsDocker/build.sh
+++ phpBinsDocker/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #
+docker buildx create --append --use --name builder-0bebb35c-3c64-437a-84a5-5461f072efd8 ubuntu
 docker buildx build --tag sirherobrine23/phpbinbuild:latest --platform linux/amd64,linux/arm64 --file dockerfile .
