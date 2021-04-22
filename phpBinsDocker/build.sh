f156a8fb75c0a2bd521534b6ab1bd47204d2300f
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-04-22 00:32:57 -0300

    Update build.sh

--- phpBinsDocker/build.sh
+++ phpBinsDocker/build.sh
@@ -4,7 +4,7 @@ git clone https://github.com/pmmp/php-build-scripts.git /tmp/php
 cd /tmp/php
 . compile.sh -j$(nproc)
 ls -la
-zip -q /tmp/out/Linux_$(uname -m | sed 's|_|-|g')_php.zip -r bin/
+zip -q /tmp/out/Linux_$(uname -m | sed 's|86-||g')_php.zip -r bin/
 
 
 exit 0
