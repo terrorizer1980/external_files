e6b46c9f737672b5fe8a6b87811e3da3360d9303
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-04-28 19:22:48 -0300

    push

--- workflows/Server.js
+++ workflows/Server.js
@@ -8,25 +8,31 @@ const Server_path = resolve(__dirname, "..", "Server.json");
 const old_Server_file = readFileSync(Server_path, "utf8");
 const new_Server = JSON.parse(old_Server_file)
 
-const url_linux = execSync(`lynx -dump "https://www.minecraft.net/en-us/download/server/bedrock" |grep 'bin-linux'| awk '/http/{print $2}'`).toString().replace("\n", "")
-const url_win = execSync(`lynx -dump "https://www.minecraft.net/en-us/download/server/bedrock" |grep 'bin-win'| awk '/http/{print $2}'`).toString().replace("\n", "")
-const pocketmine_json = JSON.parse(execSync(`curl -sS "https://api.github.com/repos/pmmp/PocketMine-MP/releases"`).toString())
-
-const server_version = url_win.split("-")[3].toString().replaceAll(".zip", "")
+const url_linux = execSync(`lynx -dump "https://www.minecraft.net/en-us/download/server/bedrock" |grep 'bin-linux'| awk '{print $2}'`).toString().replace("\n", "")
+const url_win = execSync(`lynx -dump "https://www.minecraft.net/en-us/download/server/bedrock" |grep 'bin-win'| awk '{print $2}'`).toString().replace("\n", "")
+const winSplit = url_win.split("/");
+const server_version = winSplit[(winSplit.length - 1)].replace("bedrock-server-", "").replaceAll(".zip", "")
 exportVariable("bedrock_version", server_version)
-var exist_urls;
-if (!(old_Server_file.includes(url_linux))) exist_urls = false
-else if (!(old_Server_file.includes(url_win))) exist_urls = false
-else exist_urls = true
 
-if (exist_urls){
+if (old_Server_file.includes(url_linux) && old_Server_file.includes(url_win)){
     console.log("the bedrock platform is up to date, jumping");
-}
-else {
-    execSync(`wget "${url_linux}" -O bedrock.zip && unzip -o bedrock.zip -d ./`, {cwd: "/tmp"})
-    execSync(`zip linux_libries -r $(ldd /tmp/bedrock_server|awk '{print $3}' | tr "\n" " ")`, {
-        cwd: resolve(__dirname, "..")
-    })
+} else {
+    execSync(`wget "${url_linux}" -O bedrock.zip && unzip -o bedrock.zip -d ./`, {cwd: "/tmp"});
+    var Zip = require("adm-zip");
+    Zip = new Zip();
+    const libs = execSync("ldd /tmp/bedrock_server").toString().split("\t").join("").split("\n")
+    var arrayLibs = [];
+    for (let lib of libs) {
+        lib = lib.split(/\s+/g)
+        if (lib.length === 4) arrayLibs.push(lib[2])
+        else if (lib[0].includes("/")) arrayLibs.push(lib[0])
+        else console.log(lib);
+    }
+    for (let addInzip of arrayLibs) {
+        var dir = addInzip.split("/");dir.pop(dir.length - 1);dir = dir.join("/");
+        Zip.addLocalFile(addInzip, dir);
+    }
+    Zip.writeZip(resolve(__dirname, "..", "linux_libries.zip"));
     new_Server.bedrock_latest = server_version
     let data = new Date()
     new_Server.bedrock[server_version] = {
@@ -36,28 +42,24 @@ else {
         },
         "data": `${data.getFullYear()}/${data.getMonth() +1}/${data.getDate()}`
     }
-    console.log(new_Server.bedrock);
+    console.log(new_Server.bedrock[server_version]);
 }
-const url_java = execSync(`lynx -dump "https://www.minecraft.net/en-us/download/server/" |grep "server"|grep "launcher"|awk '{print $2}'`).toString().replace("\n", "")
-const java_version = execSync(`lynx -dump "https://www.minecraft.net/en-us/download/server/"|grep "java -"|awk '{print $5}'`).toString().replace("\n", "").split("minecraft_server.").join("").split(".jar").join("")
+var java_version = execSync(`lynx -dump "https://www.minecraft.net/en-us/download/server/" | grep "java"`).toString().split(/\r?\n/g)[1].trim().split(/\s+/g).filter((data) => {if (data === "java" || data === "-jar" || data === "nogui" || data.includes("-Xm")) return false;else return true;});
+java_version = java_version[(java_version.length - 1)].replace("minecraft_server.", "").replace(".jar", "")
 exportVariable("java_version", java_version)
-var exist_urls_java;
-if (!(old_Server_file.includes(url_java))) exist_urls_java = false
-else exist_urls_java = true
-
-if (exist_urls_java){
+if (new_Server.java[java_version] !== undefined){
     console.log("the java platform is up to date, jumping");
-}
-else {
+} else {
     new_Server.java_latest = java_version
     let data = new Date()
     new_Server.java[java_version] = {
-        url: url_java,
+        url: execSync(`lynx -dump "https://www.minecraft.net/en-us/download/server/" | grep "server.jar"`).toString().split(/\s+/g).filter( data => {if (data === "" || data.match(/[0-9]+\./) ) return false;else return true;})[0],
         data: `${data.getFullYear()}/${data.getMonth() +1}/${data.getDate()}`
     }
     console.log(new_Server.java[java_version]);
 }
 
+const pocketmine_json = JSON.parse(execSync(`curl -sS "https://api.github.com/repos/pmmp/PocketMine-MP/releases"`).toString())
 new_Server.PocketMine_latest = pocketmine_json[0].tag_name
 exportVariable("pocketmine_version", pocketmine_json[0].tag_name)
 for (let index of pocketmine_json){
