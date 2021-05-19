019b04b05e2efe82274d50bed0549bf2ae4dedd1
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-05-19 13:33:57 -0300

    Push Test

---
+++ phpBinsDocker/windowsRename.js
@@ -0,0 +1,15 @@
+const { resolve, join } = require("path")
+const { readdirSync, renameSync } = require("fs");
+const { homedir } = require("os");
+
+// const _a = process.cwd()
+const _a = join(homedir(), "Downloads")
+console.log(_a);
+const fileFolder = readdirSync(_a).filter(data => {return /.zip/gi.test(data)}).filter(data => {return /vc[0-9]/gi.test(data)})
+for (let rename of fileFolder) {
+    console.log({
+        origin: join(_a, rename),
+        to: join(_a, "Windows_x64_php_with_vc_redist.zip")
+    });
+    renameSync(join(_a, rename), join(_a, "Windows_x64_php_redist.zip"))
+}
