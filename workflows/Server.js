15c69428e87dd98454e9135a450cb1545ce57d4b
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-05-26 01:05:11 -0300

    Update Server.js

--- workflows/Server.js
+++ workflows/Server.js
@@ -28,18 +28,6 @@ console.log(`Windows Bedrock URL: ${url_win}\nLinux Bedrock URL: ${url_linux}\n\
 
 // Bedrock
 if (oldServer.bedrock[BedrockServerVersion]) console.log("the bedrock platform is up to date, jumping"); else {
-    execSync(`wget "${url_linux}" -O bedrock.zip && unzip -o bedrock.zip -d ./`, {cwd: "/tmp"});
-    var Zip = require("adm-zip");
-    Zip = new Zip();
-    const libs = execSync("ldd /tmp/bedrock_server").toString().split("\t").join("").split("\n")
-    var arrayLibs = [];
-    for (let lib of libs) {lib = lib.split(/\s+/g); if (lib.length === 4) arrayLibs.push(lib[2]); else if (lib[0].includes("/")) arrayLibs.push(lib[0]); else console.log(lib);}
-    arrayLibs.push("/lib/x86_64-linux-gnu/ld-2.31.so")
-    for (let addInzip of arrayLibs) {
-        var dir = addInzip.split("/");dir.pop(dir.length - 1);dir = dir.join("/");
-        Zip.addLocalFile(addInzip, dir);
-    }
-    Zip.writeZip(resolve(__dirname, "..", "linux_libries.zip"));
     new_Server.bedrock_latest = BedrockServerVersion
     let data = new Date()
     new_Server.bedrock[BedrockServerVersion] = {
@@ -51,6 +39,19 @@ if (oldServer.bedrock[BedrockServerVersion]) console.log("the bedrock platform i
     }
     console.log(new_Server.bedrock[BedrockServerVersion]);
 }
+
+execSync(`wget "${url_linux}" -O bedrock.zip && unzip -o bedrock.zip -d ./`, {cwd: "/tmp"});
+var Zip = require("adm-zip");
+Zip = new Zip();
+const libs = execSync("ldd /tmp/bedrock_server").toString().split("\t").join("").split("\n")
+var arrayLibs = [];
+for (let lib of libs) {lib = lib.split(/\s+/g); if (lib.length === 4) arrayLibs.push(lib[2]); else if (lib[0].includes("/")) arrayLibs.push(lib[0]); else console.log(lib);}
+arrayLibs.push("/lib/x86_64-linux-gnu/ld-2.31.so")
+for (let addInzip of arrayLibs) {
+     var dir = addInzip.split("/");dir.pop(dir.length - 1);dir = dir.join("/");
+     Zip.addLocalFile(addInzip, dir);
+}
+Zip.writeZip(resolve(__dirname, "..", "linux_libries.zip"));
 
 // Java
 var java_version = javaLynx.filter(data => {return data.includes("java")}).filter(data => {return data.includes("-jar")})[0].trim().split(/\s+/g).filter((data) => {return !(data === "java" || data === "-jar" || data === "nogui" || data.includes("-Xm"))})[0].trim().split(/minecraft_server/).join("").split(/\.jar/).join("")
